ade5c5b9834a2fc301322bff256916d03e4ac034
varshittangudu1971 <tanguduvarshit071@gmail.com>
2023-07-14 18:05:14 +0530

    ok8

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1>Varshit</h1>
+        <p>?p</p>
+        </div>
+        <div>
+          hfgj
+        </div>
+  )
+}
+
+export default App;
